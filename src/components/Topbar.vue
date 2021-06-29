9a6918191bea7c10ce0650ed47d1728592d19ca8
Roman Matsypula <ukrmatsypula@gmail.com>
2021-06-29 19:53:36 +0300

    added nav when user is !isLoggedIn

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -10,6 +10,7 @@
             >Home</router-link
           >
         </li>
+
         <template v-if="isLoggedIn">
           <li class="nav-item">
             <router-link class="nav-link" :to="{ name: 'createArticle' }">
@@ -39,6 +40,20 @@
               &nbsp;
               {{ currentUser.username }}
             </router-link>
+          </li>
+        </template>
+
+        <template v-if="!isLoggedIn">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'login' }"
+              >Sign In</router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'register' }"
+              >Sign up</router-link
+            >
           </li>
         </template>
       </ul>
