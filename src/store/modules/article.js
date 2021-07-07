f575445fadc42af13fb75d6ba5d5033df0e9bade
Roman Matsypula <ukrmatsypula@gmail.com>
2021-07-07 18:36:02 +0300

    added new api articles and new store module

---
+++ src/store/modules/article.js
@@ -0,0 +1,54 @@
+import articleApi from '@/api/article'
+
+const state = {
+  data: null,
+  isLoading: false,
+  error: false,
+}
+
+export const mutationTypes = {
+  getArticleStart: '[article] getArticleStart',
+  getArticleSuccess: '[article] getArticleSuccess',
+  getArticleFailure: '[article] getArticleFailure',
+}
+
+export const actionTypes = {
+  getArticle: '[article] getArticle',
+}
+
+const mutations = {
+  [mutationTypes.getArticleStart](state) {
+    state.isLoading = true
+    state.data = null
+  },
+  [mutationTypes.getArticleSuccess](state, payload) {
+    state.isLoading = false
+    state.data = payload
+  },
+  [mutationTypes.getArticleFailure](state) {
+    state.isLoading = false
+  },
+}
+
+const actions = {
+  [actionTypes.getArticle](context, { slug }) {
+    return new Promise(resolve => {
+      context.commit(mutationTypes.getArticleStart)
+      articleApi
+        .getArticle(slug)
+        .then(article => {
+          context.commit(mutationTypes.getArticleSuccess, article)
+          resolve(article)
+        })
+        .catch(() => {
+          context.commit(mutationTypes.getArticleFailure)
+        })
+    })
+  },
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+}
