6816bdcd6a9b0a02131df06d58effe3f8c19e548
Roman Matsypula <ukrmatsypula@gmail.com>
2021-07-07 19:35:46 +0300

    render banner

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,12 +1,78 @@
 <template>
-  <div>new article</div>
+  <div class="article-page">
+    <div class="banner">
+      <div class="container" v-if="article">
+        <h1>{{ article.title }}</h1>
+        <div class="article-meta">
+          <router-link
+            :to="{
+              name: 'userProfile',
+              params: { slug: article.author.username },
+            }"
+          >
+            <img :src="article.author.image" alt="" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: { slug: article.author.username },
+              }"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <span>
+            <router-link
+              class="btn btn-outline-secondary btn-small"
+              :to="{ name: 'editArticle', params: { slug: article.slug } }"
+            >
+              <i class="ion-edit" />
+              Edit Article
+            </router-link>
+            <button class="btn btn-outline-danger btn-small">
+              <i class="ion-trash-a" />
+              Delete Article
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="container page">
+      <mcv-loading v-if="isLoading" />
+      <mcv-error-message v-if="error" :message="error" />
+      <div class="row article-content" v-if="article">
+        <div class="col-xs-12">
+          <div>
+            <p>{{ article.body }}</p>
+          </div>
+          TAGLIST
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { actionTypes } from '@/store/modules/article'
+import { mapState } from 'vuex'
+import McvLoading from '@/components/Loading'
+import McvErrorMessage from '@/components/ErrorMessage'
 
 export default {
   name: 'McvArticle',
+  components: {
+    McvLoading,
+    McvErrorMessage,
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.article.isLoading,
+      error: state => state.article.error,
+      article: state => state.article.data,
+    }),
+  },
   mounted() {
     this.$store.dispatch(actionTypes.getArticle, {
       slug: this.$route.params.slug,
