44fa224d6007b4e4efcef9132c51ce3ffa05c367
Roman Matsypula <ukrmatsypula@gmail.com>
2021-07-12 23:58:25 +0300

    added field image in our form settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,64 @@
 <template>
-  <div>Settings page</div>
+  <div class="settings-page" v-if="currentUser">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Settings</h1>
+          <mcv-validation-errors
+            v-if="validationErrors"
+            :validation-errors="validationErrors"
+          />
+          <form @submit.prevent="onSubmit">
+            <fieldset>
+              <fieldset class="form-group">
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  placeholder="URL of profile picture"
+                  v-model="form.image"
+                />
+              </fieldset>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+import { getterTypes as authGetterTypes } from '@/store/modules/auth'
+import McvValidationErrors from '@/components/ValidationErrors'
+
 export default {
   name: 'McvSettings',
+  components: {
+    McvValidationErrors,
+  },
+  computed: {
+    ...mapState({
+      isSubmitting: state => state.settings.isSubmitting,
+      validationErrors: state => state.settings.validationErrors,
+    }),
+    ...mapGetters({
+      currentUser: authGetterTypes.currentUser,
+    }),
+    form() {
+      return {
+        username: this.currentUser.username,
+        bio: this.currentUser.bio,
+        image: this.currentUser.image,
+        email: this.currentUser.email,
+        password: '',
+      }
+    },
+  },
+  methods: {
+    onSubmit() {
+      console.log('submitted settings')
+    },
+  },
+  mounted() {},
 }
 </script>
