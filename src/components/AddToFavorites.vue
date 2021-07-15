7aa3c511c96d99796e5d30ad055e59c4d0c98015
Roman Matsypula <ukrmatsypula@gmail.com>
2021-07-15 23:24:49 +0300

    finished like post

--- src/components/AddToFavorites.vue
+++ src/components/AddToFavorites.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>Add to favorites</div>
+  <button
+    @click="handleLike"
+    :class="{
+      btn: true,
+      'btn-sm': true,
+      'btn-primary': isFavoritedOptimistic,
+      'btn-outline-primary': !isFavoritedOptimistic,
+    }"
+  >
+    <i class="ion-heart"></i>
+    <span>&nbsp; {{ this.favoritesCountOptimistic }}</span>
+  </button>
 </template>
 
 <script>
+import { actionTypes } from '@/store/modules/addToFavorites'
+
 export default {
   name: 'McvAddToFavorites',
   props: {
@@ -17,6 +30,27 @@ export default {
     favoritesCount: {
       type: Number,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isFavoritedOptimistic: this.isFavorited,
+      favoritesCountOptimistic: this.favoritesCount,
+    }
+  },
+  methods: {
+    handleLike() {
+      this.$store.dispatch(actionTypes.addToFavorites, {
+        slug: this.articleSlug,
+        isFavorited: this.isFavoritedOptimistic,
+      })
+      if (this.isFavoritedOptimistic) {
+        this.favoritesCountOptimistic = this.favoritesCountOptimistic - 1
+      } else {
+        this.favoritesCountOptimistic = this.favoritesCountOptimistic + 1
+      }
+
+      this.isFavoritedOptimistic = !this.isFavoritedOptimistic
     },
   },
 }
